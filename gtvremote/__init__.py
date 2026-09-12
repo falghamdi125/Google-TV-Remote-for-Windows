325185a147e7c2037120dc6ff72abb59120f2975
Faisal Alghamdi <falghamdi125@gmail.com>
2026-09-12 06:09:30 +0300

    Version 1.2.0

--- gtvremote/__init__.py
+++ gtvremote/__init__.py
@@ -4,7 +4,7 @@ Speaks the Android TV Remote Service v2 protocol (TLS ports 6467 pairing /
 6466 control) with no third-party protocol dependencies.
 """
 
-__version__ = "1.1.1"
+__version__ = "1.2.0"
 
 import logging  # noqa: E402
 
